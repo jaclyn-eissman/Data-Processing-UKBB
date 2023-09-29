13aaad00034c9beb75d8209ca086baa9b8486b67
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-09-29 10:19:25 -0400

    Update separate_UKBB_csv.R

--- Processing/separate_UKBB_csv.R
+++ Processing/separate_UKBB_csv.R
@@ -1,12 +1,15 @@
+###By Jaclyn Eissman, 2021
+
+#Packages
 library(data.table)
 
-#directory
+#Directory
 dir <- "/Users/jackieeissman/Box Sync/Hohman_Lab/Students/Jaclyn Eissman/UKBB/files/"
 
-#read UKB excel doc
+#Read UKB excel doc
 csv <- read.csv(paste0(dir,"UKBB GWAS Imputed v3 - File Manifest Release 20180731 - Manifest 201807.csv")) #N=11934
 
-#pull males wget and write out
+#Pull males wget and write out
 males <- csv[csv$Sex=="male",]
 males <- males[,c("Phenotype.Code","File","wget.command")]
 write.table(males,paste0(dir,"UKBB_Manifest_201807_males.csv"),row.names=F,quote=F,col.names=F,sep=",") #N=3589
@@ -18,7 +21,7 @@ write.table(males_two,paste0(dir,"UKBB_Manifest_201807_males_two.csv"),row.names
 males_three <- males[2300:dim(males)[1],]
 write.table(males_three,paste0(dir,"UKBB_Manifest_201807_males_three.csv"),row.names=F,quote=F,col.names=F,sep=",") 
 
-#pull femlaes wget and write out
+#Pull femlaes wget and write out
 females <- csv[csv$Sex=="female",]
 females <- females[,c("Phenotype.Code","File","wget.command")]
 write.table(females,paste0(dir,"UKBB_Manifest_201807_females.csv"),row.names=F,quote=F,col.names=F,sep=",") #N=3748
@@ -29,4 +32,3 @@ females_two <- females[1200:2400,]
 write.table(females_two,paste0(dir,"UKBB_Manifest_201807_females_two.csv"),row.names=F,quote=F,col.names=F,sep=",")
 females_three <- females[2400:dim(females)[1],]
 write.table(females_three,paste0(dir,"UKBB_Manifest_201807_females_three.csv"),row.names=F,quote=F,col.names=F,sep=",")
-
