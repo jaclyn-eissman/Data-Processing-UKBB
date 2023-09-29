ff8d04cc48d54a147c74cbe4bf789771037b30b2
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-09-29 09:45:05 -0400

    Update create_gnova_lists.R

--- Processing/create_gnova_lists.R
+++ Processing/create_gnova_lists.R
@@ -1,9 +1,12 @@
+###By Jaclyn Eissman, 2021
+
+#Packages
 library(data.table)
 
-#directory
+#Directory
 dir <- "/Users/jackieeissman/Box Sync/Hohman_Lab/Students/Jaclyn Eissman/UKBB/files/"
 
-#read in job fail lists
+#Read in job fail lists
 F1 <- fread(paste0(dir,"failed_joblist_28674390.txt"),header=F)
 F2 <- fread(paste0(dir,"failed_joblist_28677495.txt"),header=F) 
 F3 <- fread(paste0(dir,"failed_joblist_28682475.txt"),header=F)
@@ -12,7 +15,7 @@ M1 <- fread(paste0(dir,"failed_joblist_28674391.txt"),header=F)
 M2 <- fread(paste0(dir,"failed_joblist_28680316.txt"),header=F)
 M3 <- fread(paste0(dir,"failed_joblist_28696109.txt"),header=F)
 
-#pull subjob numbers only
+#Pull subjob numbers only
 F1$V1 <- sapply(strsplit(F1$V1, "_"), "[", 2)
 F2$V1 <- sapply(strsplit(F2$V1, "_"), "[", 2)
 F3$V1 <- sapply(strsplit(F3$V1, "_"), "[", 2)
@@ -21,7 +24,7 @@ M1$V1 <- sapply(strsplit(M1$V1, "_"), "[", 2)
 M2$V1 <- sapply(strsplit(M2$V1, "_"), "[", 2)
 M3$V1 <- sapply(strsplit(M3$V1, "_"), "[", 2)
 
-#read in lists used for processing
+#Read in lists used for processing
 F1_list <- fread(paste0(dir,"Female_success_list_one.txt"),header=F)
 F2_list <- fread(paste0(dir,"Female_success_list_two.txt"),header=F)
 F3_list <- fread(paste0(dir,"Female_success_list_three.txt"),header=F)
@@ -30,7 +33,7 @@ M1_list <- fread(paste0(dir,"Male_success_list_one.txt"),header=F)
 M2_list <- fread(paste0(dir,"Male_success_list_two.txt"),header=F)
 M3_list <- fread(paste0(dir,"Male_success_list_three.txt"),header=F)
 
-#pull out the failed files from the lists
+#Pull out the failed files from the lists
 F1_list <- F1_list[!seq_len(nrow(F1_list)) %in% F1$V1,]
 F2_list <- F2_list[!seq_len(nrow(F2_list)) %in% F2$V1,]
 F3_list <- F3_list[!seq_len(nrow(F3_list)) %in% F3$V1,]
@@ -39,7 +42,7 @@ M1_list <- M1_list[!seq_len(nrow(M1_list)) %in% M1$V1,]
 M2_list <- M2_list[!seq_len(nrow(M2_list)) %in% M2$V1,]
 M3_list <- M3_list[!seq_len(nrow(M3_list)) %in% M3$V1,]
 
-#combine
+#Combine
 F_list <- rbind(F1_list,F2_list,F3_list)
 length(unique(F_list$V1))
 nrow(F1_list) + nrow(F2_list) + nrow(F3_list) 
@@ -50,8 +53,6 @@ length(unique(M_list$V1))
 nrow(M1_list) + nrow(M2_list) + nrow(M3_list) 
 M_list <- M_list[!duplicated(M_list$V1),]
 
-#write out lists for gnova
+#Write out lists for gnova
 write.table(F_list,paste0(dir,"female_trait_list.txt"),quote=F,row.names=F,col.names=F)
 write.table(M_list,paste0(dir,"male_trait_list.txt"),quote=F,row.names=F,col.names=F)
-
-
