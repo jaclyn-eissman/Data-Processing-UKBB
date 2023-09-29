fe30c6f2a9c8df790300a7face0a048e6b3f5620
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-09-29 10:04:04 -0400

    Update format_munge_both_sexes.sh

--- Processing/format_munge_both_sexes.sh
+++ Processing/format_munge_both_sexes.sh
@@ -1,31 +1,33 @@
+###By Jaclyn Eissman, 2021
+
 #!/bin/bash
 
-#set up array 
+#Set up array 
 both_sexes_file_list=$1
 
-#change directory 
+#Change directory 
 cd /data/h_vmac/eissmajm/UKBB/both_sexes
 
-#unzip file and remove zipped file
+#Unzip file and remove zipped file
 gunzip -c ${both_sexes_file_list}.tsv.bgz > ${both_sexes_file_list}.tsv
 rm ${both_sexes_file_list}.tsv.bgz
 
-#run Rscript to format
+#Run Rscript to format
 Rscript /data/h_vmac/eissmajm/UKBB/Organize_Clean_UKBB.R ${both_sexes_file_list}
 
-#check if a text file was made from Rscript, if not exit script
+#Check if a text file was made from Rscript, if not exit script
 if [[ ! -f ${both_sexes_file_list}.txt ]]; then exit 1 ; fi
 
-#remove unformatted file 
+#Remove unformatted file 
 rm ${both_sexes_file_list}.tsv
 
 ###MUNGE
 
-#load modules and source virtual environment
+#Load modules and source virtual environment
 module restore gnova_python2
 source /data/h_vmac/eissmajm/python2/bin/activate
 
-#munge trait
+#Munge trait
 /data/h_vmac/eissmajm/GNOVA-2.0/munge_sumstats.py \
 --signed-sumstats EFFECT,0 \
 --out ${both_sexes_file_list} \
@@ -36,8 +38,8 @@ source /data/h_vmac/eissmajm/python2/bin/activate
 --sumstats ${both_sexes_file_list}.txt \
 --p p-value
 
-#check if a munge file was successfully made, if not exit script
+#Check if a munge file was successfully made, if not exit script
 if [[ ! -f ${both_sexes_file_list}.sumstats.gz ]]; then exit 1 ; fi
 
-#remove formatted text file 
+#Remove formatted text file 
 rm ${both_sexes_file_list}.txt
