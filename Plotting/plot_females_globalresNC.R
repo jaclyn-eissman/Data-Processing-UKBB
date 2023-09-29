16197cf852be0e84d3005ea2790144990b7a3a51
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-09-29 10:39:24 -0400

    Update plot_females_globalresNC.R

--- Plotting/plot_females_globalresNC.R
+++ Plotting/plot_females_globalresNC.R
@@ -1,43 +1,45 @@
-#packages
+###By Jaclyn Eissman, 2021
+
+#Packages
 library(data.table)
 library(openxlsx)
 library(ggplot2)
 library(ggrepel)
 
-#directory
+#Directory
 dir <- "/Users/jackieeissman/Box Sync/Hohman_Lab/Students/Jaclyn Eissman/UKBB/"
 
-#read in excel file with groups
+#Read in excel file with groups
   #unique(excel$Phenotype.Group) -- looks like all groups are filled in for all phenotypes
 excel <- read.xlsx(paste0(dir,"UKBB_all_with_pheno_groups_v2.xlsx"))
 
-#subset to females
+#Subset to females
 females <- excel[excel$Sex=="female",]
 
-#remove the extensions from file name
+#Remove the extensions from file name
 females$File <- gsub(".tsv.bgz","",females$File)
 
-#read in example data
+#Read in example data
 globalresNC <- read.table(paste0(dir,"results/FEMALES.GLOBALRES_NC.ALL.txt"),header=T)
 globalresNC$trait <- gsub(".globalresNC.txt","",globalresNC$trait)
 globalresNC$trait <- gsub("[[:space:]]+$", "",globalresNC$trait)
 
-#merge 
+#Merge 
 globalresNC <- merge(globalresNC,females,by.x="trait",by.y="File")
 globalresNC <- globalresNC[order(globalresNC$pvalue_corrected),]
 globalresNC$P.Fdr <- p.adjust(globalresNC$pvalue_corrected,method="fdr")
 
-#pull traits that survive FDR...and see which ones I want to label
+#Pull traits that survive FDR...and see which ones I want to label
 sig <- globalresNC[globalresNC$P.Fdr<0.05,]
 sig <- sig[,c("Phenotype.Group","Phenotype.Description","pvalue_corrected","P.Fdr")]
 sig <- sig[order(sig$Phenotype.Group,sig$P.Fdr),]
 write.table(sig,paste0(dir,"results/FEMALES_GLOBALRES_NC_FDR_survive.txt"),quote=F,col.names=T,row.names=F)
 to_label <- c()
 
-#add binary variable for pos or neg assoc
+#Add binary variable for pos or neg assoc
 globalresNC$direction <- ifelse(globalresNC$rho_corrected>0,1,2)
 
-#plotting 
+#Plotting 
 png(paste0(dir,"plots/FEMALES_GLOBALRES_NC.png"),width=13,height=7.5,units="in",res=1000)
 ggplot(globalresNC, aes(x=Phenotype.Group, y=-log10(pvalue_corrected), color=Phenotype.Group, shape=as.factor(direction))) + 
   geom_jitter() + theme_bw() + geom_hline(yintercept=-log10(2.861710e-03), linetype="dashed") + 
@@ -46,4 +48,3 @@ ggplot(globalresNC, aes(x=Phenotype.Group, y=-log10(pvalue_corrected), color=Phe
   geom_text_repel(aes(label=ifelse(Phenotype.Description %in% to_label,Phenotype.Description,"")),max.overlaps=Inf) +
   scale_shape_manual(values=c(2,6)) 
 dev.off()
-
