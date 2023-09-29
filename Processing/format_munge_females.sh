dae154a32d65b105ef0c7b12349df70fcca33988
Jaclyn <65563725+jaclyn-eissman@users.noreply.github.com>
2023-09-29 10:17:44 -0400

    Update format_munge_females.sh

--- Processing/format_munge_females.sh
+++ Processing/format_munge_females.sh
@@ -1,31 +1,33 @@
+###By Jaclyn Eissman, 2021
+
 #!/bin/bash
 
-#set up array 
+#Set up array 
 female_file_list=$1
 
-#change directory 
+#Change directory 
 cd /data/h_vmac/eissmajm/UKBB/females
 
-#unzip file and remove zipped file
+#Unzip file and remove zipped file
 gunzip -c ${female_file_list}.tsv.bgz > ${female_file_list}.tsv
 rm ${female_file_list}.tsv.bgz
 
-#run Rscript to format
+#Run Rscript to format
 Rscript /data/h_vmac/eissmajm/UKBB/Organize_Clean_UKBB.R ${female_file_list}
 
-#check if a text file was made from Rscript, if not exit script
+#Check if a text file was made from Rscript, if not exit script
 if [[ ! -f ${female_file_list}.txt ]]; then exit 1 ; fi
 
-#remove unformatted file 
+#Remove unformatted file 
 rm ${female_file_list}.tsv
 
 ###MUNGE
 
-#load modules and source virtual environment
+#Load modules and source virtual environment
 module restore gnova_python2
 source /data/h_vmac/eissmajm/python2/bin/activate
 
-#munge trait
+#Munge trait
 /data/h_vmac/eissmajm/GNOVA/munge_sumstats.py \
 --signed-sumstats EFFECT,0 \
 --out ${female_file_list} \
@@ -36,8 +38,8 @@ source /data/h_vmac/eissmajm/python2/bin/activate
 --sumstats ${female_file_list}.txt \
 --p p-value
 
-#check if a munge file was successfully made, if not exit script
+#Check if a munge file was successfully made, if not exit script
 if [[ ! -f ${female_file_list}.sumstats.gz ]]; then exit 1 ; fi
 
-#remove formatted text file 
+#Remove formatted text file 
 rm ${female_file_list}.txt
